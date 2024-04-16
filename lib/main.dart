b4f0fb03ae6888000b24f723915df30ea8cdce2d
jwhubert91 <jwhubert91@gmail.com>
2024-04-16 16:12:28 -0500

    add sandbox component

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,37 @@ import 'package:coffee_card/home.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Home(),
+    home: Sandbox(),
   ));
+}
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: Column(children: [
+        Container(
+          width: 100,
+          color: Colors.red,
+          child: const Text('one'),
+        ),
+        Container(
+          width: 200,
+          color: Colors.green,
+          child: const Text('one'),
+        ),
+        Container(
+          width: 300,
+          color: Colors.blue,
+          child: const Text('one'),
+        )
+      ]),
+    );
+  }
 }
