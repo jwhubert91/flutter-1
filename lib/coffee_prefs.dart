33105b6484a81e7228b081e40d060be5bb9a7f27
jwhubert91 <jwhubert91@gmail.com>
2024-04-17 16:36:40 -0500

    stateful widget

--- lib/coffee_prefs.dart
+++ lib/coffee_prefs.dart
@@ -1,28 +1,68 @@
 import 'package:flutter/material.dart';
 
-class CoffeePrefs extends StatelessWidget {
+class CoffeePrefs extends StatefulWidget {
   const CoffeePrefs({super.key});
 
   @override
+  State<CoffeePrefs> createState() => _CoffeePrefsState();
+}
+
+class _CoffeePrefsState extends State<CoffeePrefs> {
+  int strength = 1;
+  int sugars = 1;
+
+  void increaseStrength() {
+    setState(() {
+      strength = strength < 5 ? strength + 1 : 1;
+    });
+  }
+
+  void increaseSugars() {
+    setState(() {
+      sugars = sugars < 5 ? sugars + 1 : 1;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
         Row(
           children: [
-            Text("Strength: "),
-            Text('3'),
-            SizedBox(
-              width: 50,
+            const Text("Strength: "),
+            Text('$strength'),
+            Image.asset(
+              "assets/images/coffee_bean.png",
+              width: 25,
+              color: Colors.brown[100],
+              colorBlendMode: BlendMode.multiply,
+            ),
+            const Expanded(child: SizedBox()),
+            FilledButton(
+              onPressed: increaseStrength,
+              style: FilledButton.styleFrom(
+                  backgroundColor: Colors.brown, foregroundColor: Colors.white),
+              child: const Text("+"),
             ),
           ],
         ),
         Row(
           children: [
-            Text("Sugars: "),
-            Text('3'),
-            SizedBox(
-              width: 50,
+            const Text("Sugars: "),
+            Text('$sugars'),
+            Image.asset(
+              "assets/images/sugar_cube.png",
+              width: 25,
+              color: Colors.brown[100],
+              colorBlendMode: BlendMode.multiply,
             ),
+            const Expanded(child: SizedBox()),
+            FilledButton(
+                style: FilledButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                    foregroundColor: Colors.white),
+                onPressed: increaseSugars,
+                child: const Text("+"))
           ],
         ),
       ],
--- lib/home.dart
+++ lib/home.dart
@@ -30,6 +30,13 @@ class Home extends StatelessWidget {
             padding: const EdgeInsets.all(20),
             child: const CoffeePrefs(),
           ),
+          Expanded(
+            child: Image.asset(
+              "assets/images/coffee_bg.jpg",
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.bottomCenter,
+            ),
+          ),
         ],
       ),
     );
